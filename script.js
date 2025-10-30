a2a7c1558443949c2f72ab9f3355dee2a951c29f
Vini <viniciuszucatti@gmail.com>
2025-10-30 17:05:42 -0300

    Ajustes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-(async function(){
+(async function () {
   const app = document.getElementById('app');
   const res = await fetch('config.json');
   const cfg = await res.json();
@@ -6,18 +6,18 @@
   document.getElementById('name').textContent = cfg.profile.name;
   document.getElementById('bio').textContent = cfg.profile.bio;
   const avatar = document.getElementById('avatar');
-  if(cfg.profile.avatar){
+  if (cfg.profile.avatar) {
     avatar.src = cfg.profile.avatar;
-    avatar.style.display='inline-block';
+    avatar.style.display = 'inline-block';
   }
 
   const linksEl = document.getElementById('links');
-  cfg.links.forEach(l => {
+  cfg.links.forEach((l) => {
     const a = document.createElement('a');
     a.href = l.url;
     a.className = 'link-card';
     a.target = '_blank';
-    if(l.image){
+    if (l.image) {
       const img = document.createElement('img');
       img.src = l.image;
       img.className = 'link-image';
@@ -29,6 +29,6 @@
     linksEl.appendChild(a);
   });
 
-  document.getElementById('footer').textContent = cfg.footer;
+  // document.getElementById('footer').textContent = cfg.footer;
   app.hidden = false;
 })();
